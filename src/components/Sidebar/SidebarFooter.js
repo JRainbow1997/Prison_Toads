110a6b63620dfb9f504d4e9628c07934f931092f
Emily <e.charlotte94@gmail.com>
2020-10-28 17:27:24 +0000

    Added sidebar with suggested frogs

---
+++ src/components/Sidebar/SidebarFooter.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './SidebarFooter.css';
+
+const SidebarFooter = () => {
+    
+   return(
+       <div className = "sidebar-footer">   
+         <ul className="footer-links">
+            <li href="">About · </li>
+            <li href="">Help · </li>
+            <li href="">Press · </li>
+            <li href="">API · </li>
+            <li href="">Jobs · </li>
+            <li href="">Privacy · </li>
+            <li href="">Terms · </li>
+            <li href="">Locations · </li>
+            <li href="">Top Accounts · </li>
+            <li href="">Hashtags · </li>
+            <li href="">Language · </li>
+         </ul>      
+         <p>© 2020 TOADSTAGRAM FROM WARTBOOK</p>
+       </div>
+   )
+
+}
+
+export default SidebarFooter
