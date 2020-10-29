ccc2afd6cde3fe3f85eec836523013341fed7820
Emily <e.charlotte94@gmail.com>
2020-10-29 16:06:05 +0000

    Added interactivity to follow buttons in sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -10,59 +10,61 @@ import frog5 from './img/profile-pic-5.png';
 import frog6 from './img/profile-pic-6.png';
 
 
-class Sidebar extends React.Component {
-   state = {}
-   render() { 
-      return (
-         <div className="all">
-            <div className="sidebarWrap">
+const Sidebar = () => {
+   return (
+      <div className="all">
+         <div className="sidebarWrap">
 
-               <div className="userInfo">
-                  <img id="userProfilePic" src={frog1} alt="" />
-                  <span className="userInfoText">
-                     <a href="" id="username">MrToad55rbit</a>
-                     <p id="nametag">Millionaire toad | USA</p>
-                  </span>
-               </div>
-
-               <span className="suggestionBanner">
-                  <p>Suggestions for You</p>
-                  <a href="">See All</a>
+            <div className="userInfo">
+               <img id="userProfilePic" src={frog1} alt="" />
+               <span className="userInfoText">
+                  <a href="" id="username">MrToad55rbit</a>
+                  <p id="nametag">Millionaire toad | USA</p>
                </span>
-
-               <SidebarUsers 
-                  profilePicSuggestions={frog2}
-                  commonFollowers="@userfrog1"
-                  commonFollowersLength="9"
-               />
-               <SidebarUsers 
-                  profilePicSuggestions={frog3}
-                  commonFollowers="@userfrog2"
-                  commonFollowersLength="9"
-               />
-               <SidebarUsers 
-                  profilePicSuggestions={frog4}
-                  commonFollowers="@userfrog3"
-                  commonFollowersLength="9"
-               />
-               <SidebarUsers 
-                  profilePicSuggestions={frog5}
-                  commonFollowers="@userfrog4"
-                  commonFollowersLength="9"
-               />
-               <SidebarUsers 
-                  profilePicSuggestions={frog6}
-                  commonFollowers="@userfrog5"
-                  commonFollowersLength="9"
-               />
-
-               <SidebarFooter />
-               
-
             </div>
+
+            <span className="suggestionBanner">
+               <p>Suggestions for You</p>
+               <a href="">See All</a>
+            </span>
+
+            <SidebarUsers
+               profilePicSuggestions={frog2}
+               suggestedUserName="toad_on_the_edge"
+               commonFollowers="@wild_toad"
+               commonFollowersLength="5"
+            />
+            <SidebarUsers
+               profilePicSuggestions={frog3}
+               suggestedUserName="Kermy-101"
+               commonFollowers="@miss-piggy-gold"
+               commonFollowersLength="1"
+            />
+            <SidebarUsers
+               profilePicSuggestions={frog4}
+               suggestedUserName="battle-toad-gains"
+               commonFollowers="@zitz87, @pimple-z"
+               commonFollowersLength="9"
+            />
+            <SidebarUsers
+               profilePicSuggestions={frog5}
+               suggestedUserName="Gentlefrog42"
+               commonFollowers="@Herm77"
+               commonFollowersLength="2"
+            />
+            <SidebarUsers
+               profilePicSuggestions={frog6}
+               suggestedUserName="MichiganJ195"
+               commonFollowers="@RRunner49"
+               commonFollowersLength="8"
+            />
+
+            <SidebarFooter />
+
+
          </div>
-      );
-   }
+      </div>
+   );
 }
- 
+
 export default Sidebar;
--- src/components/Sidebar/SidebarUsers.js
+++ src/components/Sidebar/SidebarUsers.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import './SidebarUsers.css';
 
-const SidebarFooter = (props) => {
+const SidebarUsers = (props) => {
+
+   const follow = (event) => {
+      // event.target.innerHTML = "Following"
+      
+      
+      if (event.target.innerHTML==="Follow") {
+         event.target.innerHTML = "Following"
+      } else if (event.target.innerHTML==="Following"){
+         event.target.innerHTML = "Follow"
+      }
+   }
     
    return(
        <div className = "suggestedUsers">   
          <img src={props.profilePicSuggestions} alt="" id="suggestedProfilePic"  />
          <span className="suggestedUsersText">
-          <a href="">username</a>
+          <a href="">{props.suggestedUserName}</a>
           <p>Followed by {props.commonFollowers} + {props.commonFollowersLength} more</p></span>
-          <button href="" id="followButton">Follow</button>
-         
-       </div>
+          <button href="" id="followButton" onClick={follow}>Follow</button>
+         </div>
+       
    )
 
 }
 
-export default SidebarFooter
+export default SidebarUsers
