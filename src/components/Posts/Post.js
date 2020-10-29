d02a551da8e6b41d842dddf583f5ad6b83056060
Madeline <madeline.harrop@gmail.com>
2020-10-29 16:59:21 +0000

    end of day meatball button

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../Posts/Post.css';
 import Comment from './Comment'
-import Meatball from './Meatball'
+// import Dropdown from 'react-dropdown';
 import { FaRegComment } from 'react-icons/fa';
 import { FiSend } from 'react-icons/fi';
 import { FiBookmark } from 'react-icons/fi';
@@ -9,17 +9,27 @@ import { FiBookmark } from 'react-icons/fi';
 
 
 const Post = (props) => {
-    
-    return(
-        <div className ="post">
+
+    // const options = [
+    //     'one', 'two', 'three'
+    // ];
+
+    // const defaultOption = options[0];
+
+
+    return (
+        <div className="post">
             <div className="postTop">
                 <div className="icon-name">
                     <h3><img className="icon" src="https://magedalqerbi.cachefly.net/p/20009/9.webp" /> Kermit</h3>
                 </div>
-                <Meatball />
+
+
+                {/* <Dropdown options={options} onChange={this._onSelect} value={defaultOption} placeholder="Select an option" />; */}
+
                 {/* <div className="button-meatball">
                     <button className="postDropDown"><img className="meatballMenu" src="https://www.mathmlcentral.com/characters/glyphs/TripleDot_L.gif" alt="trippledot"/></button>
-                </div> */}
+                </div>  */}
             </div>
 
             <div>
@@ -27,28 +37,28 @@ const Post = (props) => {
                     <img className="postPicture" src="https://cdn131.picsart.com/318968072360201.jpg?type=webp&to=crop&r=256" />
 
                     <div className="postIcons">
-                    <div className="icon1-3">
-                        <button type="submit" className="wartsbutton" onClick= {props.addToWart}>🐸</button>
+                        <div className="icon1-3">
+                            <button type="submit" className="wartsbutton" onClick={props.addToWart}>🐸</button>
 
-                        <p><FaRegComment /></p>
-                        <p><FiSend /> </p>
-                    </div> 
+                            <p><FaRegComment /></p>
+                            <p><FiSend /> </p>
+                        </div>
                         <p className="bookmark"><FiBookmark /></p>
                     </div>
-                    
+
                 </div>
 
                 <p><span>{props.warts}</span>  Warts</p>
 
                 <div className="postDesciption">
-                    <p><a  className="username" href="google.com"> Kermit </a> Just chilling at pride </p>
+                    <p><a className="username" href="google.com"> Kermit </a> Just chilling at pride </p>
                 </div>
 
-                <Comment />                
-                                           
+                <Comment />
+
             </div>
-        </div>            
-        
+        </div>
+
     )
 
 }
