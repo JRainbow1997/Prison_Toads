30fbc5a40172a1b3a2170f10901d236313e72e3f
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2020-10-30 10:19:25 +0000

    Photos now show up on lightbox

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -14,15 +14,15 @@ import flowertoad from './images/flowertoad.jpg';
 import africanfrog from './images/africanfrog.jpg';
 
 const lightboximages = [
-    './images/tiedyetoad.jpg',
-    './images/tonguefrog.jpg',
-    './images/toadthroat.jpg',
-    './images/peacetoad.jpg',
-    './images/winetoad.jpg',
-    './images/toadnmouse.jpg',
-    './images/jumpingtoad.jpg',
-    './images/flowertoad.jpg',
-    './images/africanfrog.jpg'
+    'https://nypost.com/wp-content/uploads/sites/2/2019/09/toadimage.jpg?quality=80&strip=all',
+    'https://i.pinimg.com/originals/2f/be/f6/2fbef6cbc36d79fd310d83cb2a897bc4.jpg',
+    'https://3.bp.blogspot.com/-T9pQ0Iy3Qs4/T2jymXfVRSI/AAAAAAAAOFE/AeooqSVIL0U/s1600/3.jpg',
+    'https://i.pinimg.com/originals/87/5a/0e/875a0ed81e345e5d45edb19fc2e60748.jpg',
+    'https://www.sallymitchell.com/prodimg/13696-F_1_Zoom.jpg',
+    'https://www.nationalgeographic.com/content/dam/news/rights-exempt/legacy-news/2006/07/060705-mouse-frog_big.ngsversion.1533652795679.adapt.1900.1.jpg',
+    'https://i.pinimg.com/originals/27/05/63/2705636c2cd1cff4646a4c9b9a873a66.jpg',
+    'https://i.pinimg.com/736x/1e/36/0c/1e360c324da7891d303389a95efacda1.jpg',
+    'https://www.thesprucepets.com/thmb/0mGrChXtmzuoDOYE1Y3CbbntRIA=/1883x1412/smart/filters:no_upscale()/GettyImages-148611850-584f93145f9b58a8cd106c13.jpg'
 ];
 
 class Stories extends Component {
@@ -52,7 +52,7 @@ class Stories extends Component {
 
     render() {
 
-        const allStories = this.state.stories.map( (story) => {
+        const allStories = this.state.stories.map((story) => {
             return (
                 <Story storyImage={story.storyImage} onClick={this.handleShowLargeImage} />
             )
@@ -63,9 +63,13 @@ class Stories extends Component {
         return (
             <div className="main-stories-container">
                 <div className="stories-container">
-                    <button type="button" onClick={() => this.setState({
+                    <button className="stories-button" type="button" onClick={() => this.setState({
                         isOpen: true
-                    })}>Open</button>
+                    })}>                    
+                        <div className="image-frame">
+                            { allStories }
+                        </div>
+                    </button>
                     {isOpen && (
                         <Lightbox
                             mainSrc={lightboximages[photoIndex]}
@@ -84,9 +88,6 @@ class Stories extends Component {
                     }
                     />
                     )}
-                    <div className="image-frame">
-                        { allStories }
-                    </div>
                 </div>
             </div>
         )
